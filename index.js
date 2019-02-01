8429244a967fd33215d1f51c0012aa175e4c72ac
zhangchenguang <zhangchenguang@163.com>
2019-02-01 17:00:00 +0800

    ceshi

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const tableRouter=require("./routes/admin/table");
 
 
 //创建HTTP应用服务器
-var app=express();
-app.listen(PORT,()=>{
+var app=express();   
+app.listen(PORT,()=>{   
   console.log("Server Listening:"+PORT+"...")
 });
 //使用cors中间件解决跨域 ;位置：服务器后挂载路由前
